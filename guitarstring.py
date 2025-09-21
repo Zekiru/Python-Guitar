e1b8891a435389307156ca28a2b7c567dde4be99
Zekiru <zekiel.villasurda@gmail.com>
2025-09-22 00:36:29 +0800

    Implement GuitarString

--- guitarstring.py
+++ guitarstring.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python3
 
+import random
+from math import ceil
+from ringbuffer import RingBuffer
+
+SAMP_RATE = 44100
+DECAY = 0.996
+
 class GuitarString:
     def __init__(self, frequency: float):
-        '''
-        Create a guitar string of the given frequency, using a sampling rate of 44100 Hz
-        '''
-        # TO-DO: implement this
-        self.capacity = # TO-DO: compute the max capacity of the ring buffer based on the frequency
-        self.buffer =   # TO-DO: construct the ring buffer object
+        # Create a guitar string of the given frequency, using a sampling rate of 44100 Hz
+        self.tick_count = 0
+        self.capacity = ceil(SAMP_RATE/frequency)
+        self.buffer = RingBuffer(self.capacity)
+        for _ in range(self.capacity): self.buffer.enqueue(0)
 
     @classmethod
     def make_from_array(cls, init: list[int]):
-        '''
-        Create a guitar string whose size and initial values are given by the array `init`
-        '''
+        # Create a guitar string whose size and initial values are given by the array `init`
         # create GuitarString object with placeholder freq
         stg = cls(1000)
 
@@ -24,25 +28,22 @@ class GuitarString:
         return stg
 
     def pluck(self):
-        '''
-        Set the buffer to white noise
-        '''
-        # TO-DO: implement this
+        # Set the buffer to white noise
+        for _ in range(self.buffer.size()): self.buffer.dequeue()
+        for _ in range(self.capacity): self.buffer.enqueue(random.uniform(-0.5, 0.5))
 
     def tick(self):
-        '''
-        Advance the simulation one time step by applying the Karplus--Strong update
-        '''
-        # TO-DO: implement this
+        # Advance the simulation one time step by applying the Karplus--Strong update
+        sample_1 = self.buffer.dequeue()
+        sample_2 = self.buffer.peek()
+
+        self.buffer.enqueue(DECAY * (sample_1 + sample_2) / 2)
+        self.tick_count += 1
 
     def sample(self) -> float:
-        '''
-        Return the current sample
-        '''
-        # TO-DO: implement this
+        # Return the current sample
+        return self.buffer.peek()
 
     def time(self) -> int:
-        '''
-        Return the number of ticks so far
-        '''
-        # TO-DO: implement this
+        # Return the number of ticks so far
+        return self.tick_count
